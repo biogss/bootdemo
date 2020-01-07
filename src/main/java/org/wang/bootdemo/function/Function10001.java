2aabe8dd67acfceb95eb2dc3902f4ca79176e7fc
biogss <2855971203@qq.com>
2020-01-07 22:42:47 +0800

    添加登录方法

---
+++ src/main/java/org/wang/bootdemo/function/Function10001.java
@@ -0,0 +1,11 @@
+package org.wang.bootdemo.function;
+
+/**
+ * @ClassName Function10001
+ * @Description 用户登录
+ * @Author wangHao
+ * @DATE 2020/1/7 22:41
+ * @Version 1.0
+ */
+public class Function10001 {
+}
